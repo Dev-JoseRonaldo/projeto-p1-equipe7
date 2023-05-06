9d2e47cd13fa968565fcad86ca05587827e07ddb
bomday <dayanecamilelima@gmail.com>
2023-05-06 16:53:22 -0300

    feat: add menu screen

--- assets/screens/game_over.py
+++ assets/screens/game_over.py
@@ -3,10 +3,34 @@ import pygame
 class GameOver():
     pygame.init()
 
-    def __init__(self, screen, score, powerups):
+    def __init__(self, screen, score, powerups, width_button, height_button, width_powerup, height_powerup, pos_x_game_over, pos_y_game_over, pos_x_score, pos_y_score, pos_x_powerup1, pos_x_powerup2, pos_x_powerup3, pos_y_powerup1, pos_y_powerup2, pos_y_powerup3, pos_x_label_powerup1, pos_x_label_powerup2, pos_x_label_powerup3, pos_y_label_powerup1, pos_y_label_powerup2, pos_y_label_powerup3, pos_x_restart, pos_y_restart, pos_x_menu, pos_y_menu):
         self.screen = screen #tela
         self.score = score #score final
-        self.powerups = powerups #powerups 
+        self.powerups = powerups #lista de powerups coletados
+        self.width_button = width_button #largura botão
+        self.height_button = height_button #altura botão
+        self.width_powerup = width_powerup #largura imagem powerup
+        self.height_powerup = height_powerup #altura imagem powerup
+        self.pos_x_game_over = pos_x_game_over #posição no eixo x da mensagem 'GAME OVER'
+        self.pos_y_game_over = pos_y_game_over #posição no eixo y da mensagem 'GAME OVER'
+        self.pos_x_score = pos_x_score #posição no eixo x do Score
+        self.pos_y_score = pos_y_score #posição no eixo y do Score
+        self.pos_x_powerup1 = pos_x_powerup1 #posição no eixo x da imagem do powerup 1
+        self.pos_x_powerup2 = pos_x_powerup2 #posição no eixo x da imagem do powerup 2
+        self.pos_x_powerup3 = pos_x_powerup3 #posição no eixo x da imagem do powerup 3
+        self.pos_y_powerup1 = pos_y_powerup1 #posição no eixo y da imagem do powerup 1
+        self.pos_y_powerup2 = pos_y_powerup2 #posição no eixo y da imagem do powerup 2
+        self.pos_y_powerup3 = pos_y_powerup3 #posição no eixo y da imagem do powerup 3
+        self.pos_x_label_powerup1 = pos_x_label_powerup1 #posição no eixo x da mensagem do powerup 1
+        self.pos_x_label_powerup2 = pos_x_label_powerup2 #posição no eixo x da mensagem do powerup 2
+        self.pos_x_label_powerup3 = pos_x_label_powerup3 #posição no eixo x da mensagem do powerup 3
+        self.pos_y_label_powerup1 = pos_y_label_powerup1 #posição no eixo y da mensagem do powerup 1
+        self.pos_y_label_powerup2 = pos_y_label_powerup2 #posição no eixo y da mensagem do powerup 2
+        self.pos_y_label_powerup3 = pos_y_label_powerup3 #posição no eixo y da mensagem do powerup 3
+        self.pos_x_restart = pos_x_restart #posição no eixo x do botão restart
+        self.pos_y_restart = pos_y_restart #posição no eixo y do botão restart
+        self.pos_x_menu = pos_x_menu #posição no eixo x do botão menu
+        self.pos_y_menu = pos_y_menu #posição no eixo y do botão menu
                 
     def game_over_screen(self):
         #modelo de mensagem
@@ -16,23 +40,23 @@ class GameOver():
             message_complete = message_font.render(message_screen, True, color)
             return message_complete
     
-        self.screen.fill((255,255,255))
+        self.screen.fill((255,255,255)) #colocar background
 
         #botões
-        img_play_button = pygame.image.load('./assets/sprites/botao.png')
+        img_restart_button = pygame.image.load('./assets/sprites/botao.png')
         img_menu_button = pygame.image.load('./assets/sprites/botao.png')
 
-        play = pygame.transform.scale(img_play_button, (150,100))
-        menu = pygame.transform.scale(img_menu_button, (150,100))
+        restart = pygame.transform.scale(img_restart_button, (self.width_button,self.height_button))
+        menu = pygame.transform.scale(img_menu_button, (self.width_button,self.height_button))
 
         #coletáveis
         img_powerup1 = pygame.image.load('./assets/sprites/powerups/apple.png')
         img_powerup2 = pygame.image.load('./assets/sprites/powerups/avocado.png')
         img_powerup3 = pygame.image.load('./assets/sprites/powerups/watermelon.png')
 
-        powerup1 = pygame.transform.scale(img_powerup1, (40,40))
-        powerup2 = pygame.transform.scale(img_powerup2, (40,40))
-        powerup3 = pygame.transform.scale(img_powerup3, (40,40))
+        powerup1 = pygame.transform.scale(img_powerup1, (self.width_powerup,self.height_powerup))
+        powerup2 = pygame.transform.scale(img_powerup2, (self.width_powerup,self.height_powerup))
+        powerup3 = pygame.transform.scale(img_powerup3, (self.width_powerup,self.height_powerup))
 
         #mensagens
         message_game_over = message_screen('GAME OVER', 50, (0,0,0))
@@ -42,16 +66,16 @@ class GameOver():
         message_powerup3 = message_screen(f'x {self.powerups[2]}', 30, (0,0,0)) #colocar varíavel de pontos no local do 00 
 
         #apresenta na tela
-        self.screen.blit(message_game_over, (200,160))
-        self.screen.blit(message_score, (260, 245))
+        self.screen.blit(message_game_over, (self.pos_x_game_over,self.pos_y_game_over))
+        self.screen.blit(message_score, (self.pos_x_score, self.pos_y_score))
 
-        self.screen.blit(powerup1, (180,290))
-        self.screen.blit(message_powerup1, (230, 290))
-        self.screen.blit(powerup2, (300,290))
-        self.screen.blit(message_powerup2, (350, 290))
-        self.screen.blit(powerup3, (420,290))
-        self.screen.blit(message_powerup3, (470, 290))
+        self.screen.blit(powerup1, (self.pos_x_powerup1,self.pos_y_powerup1))
+        self.screen.blit(message_powerup1, (self.pos_x_label_powerup1, self.pos_y_label_powerup1))
+        self.screen.blit(powerup2, (self.pos_x_powerup2,self.pos_y_powerup2))
+        self.screen.blit(message_powerup2, (self.pos_x_label_powerup2, self.pos_y_label_powerup2))
+        self.screen.blit(powerup3, (self.pos_x_powerup3,self.pos_y_powerup3))
+        self.screen.blit(message_powerup3, (self.pos_x_label_powerup3, self.pos_y_label_powerup3))
 
-        self.screen.blit(play, (270,400))
-        self.screen.blit(menu, (270,480))
+        self.screen.blit(restart, (self.pos_x_restart,self.pos_y_restart))
+        self.screen.blit(menu, (self.pos_x_menu,self.pos_y_menu))
         
